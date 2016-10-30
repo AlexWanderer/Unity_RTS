5679684b40fa6d516a5c8e7d2d0bef0efe5bf614
KaRKa <seskusk@gmail.com>
2016-10-30 12:47:32 +0000

    Update to initial project
    Added a basic build mode script.

---
+++ RTS/Assets/Scripts/BuildModeController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildModeController : MonoBehaviour {
+
+    private GameObject gridObject;
+    
+    void Awake () // Function runs once before start
+    {
+        gridObject = GameObject.FindGameObjectWithTag("Grid"); // Finds the grid object
+    }
+
+	void Start () // Function runs once 
+    {
+        gridObject.SetActive(false); // Deactivates the grid object in the scene
+	}
+	
+	void Update () // Function runs every frame
+    {
+        if (Input.GetKeyDown(KeyCode.B)) // Checks for input
+        {
+            gridObject.SetActive(!gridObject.activeSelf); // Sets the current active state to the opposite value
+        }
+	}
+}
